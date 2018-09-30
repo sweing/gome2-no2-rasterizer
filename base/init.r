2f5ca166d6cad6cac65441d203ae017fa78ffc64
Stefan Weingärtner <>
2018-09-30 14:41:29 +0200

    Commiting base files

---
+++ base/init.r
@@ -0,0 +1,12 @@
+source("./env.r")
+source("./helpers.r")
+source("./globals.r")
+source(".auth.txt") #ADD YOUR LOGIN DATA HERE
+
+requiredPackages = c("data.table")
+
+if (javaInstalled)
+    requiredPackages = c(requiredPackages, "xlsx")
+
+loadPackages(requiredPackages)
+
